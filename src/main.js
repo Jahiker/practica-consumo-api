cee234823a0f9af131b6bfddbe9333b0712592ad
jahiker <rojasjahiker@gmail.com>
2022-12-17 23:56:36 -0400

    [add] preview trending section

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-console.log("Init")
+async function getTrendingMoviesPreview() {
+    const res = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY)
+    const data = await res.json();
+    const movies = data.results;
+
+    console.log({data, movies});
+
+    movies.forEach(movie => {
+        const trendingPreviewMovieContainer = document.querySelector("#trendingPreview .trendingPreview-movieList");
+        const movieContainer = document.createElement("div");
+        movieContainer.classList.add("movie-container");
+
+        const movieImg = document.createElement("img");
+        movieImg.classList.add("movie-img");
+        movieImg.alt = movie.title;
+        movieImg.src = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
+
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMovieContainer.appendChild(movieContainer);
+    });
+}
+
+getTrendingMoviesPreview();
